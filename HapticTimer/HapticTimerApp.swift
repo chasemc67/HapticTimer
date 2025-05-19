352fd18427e9af0e9cb1e74bdb369d323c1ab52e
Chase McCarty <chasemc67@gmail.com>
2025-05-18 18:51:15 -0700

    Initial Commit

---
+++ HapticTimer/HapticTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticTimerApp.swift
+//  HapticTimer
+//
+//  Created by Chase McCarty on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct HapticTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HapticTimerTests/HapticTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HapticTimerTests.swift
+//  HapticTimerTests
+//
+//  Created by Chase McCarty on 5/18/25.
+//
+
+import Testing
+@testable import HapticTimer
+
+struct HapticTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
